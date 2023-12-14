a3bf5fe1d47b8367efd6c9ea20fac04efa7c5135
DELL <nirpansuriya@gmail.com>
2023-12-14 17:07:15 +0530

    data type.py

---
+++ module-1/data type.py
@@ -0,0 +1,24 @@
+print("\t|| python advantedge ||")
+print("store value in variable without declare variable")
+print("store long integer no limit in value")
+a=123456789012345678901234567890
+b=3.14
+c="nirav pansuriya"
+d=True
+e=67j
+print("\t|| print value in diferent data type value ||")
+
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
+print("d = ",d)
+print("e = ",e)
+
+print("\t|| print value data type ||")
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
