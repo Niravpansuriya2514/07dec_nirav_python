e32a7a29964582a87ffc7756512b083f726037bc
DELL <nirpansuriya@gmail.com>
2023-12-27 12:36:42 +0530

    module-1

---
+++ module-1/conditional statements/task.py
@@ -0,0 +1,22 @@
+
+i=int(input("enter your number : "))
+
+if i%2==0:
+    print("a is even")
+else:
+    print("a i odd")
+a=int(input("enter your number : "))
+b=int(input("enter your number : "))
+c=int(input("enter your number : "))
+
+if a>b and a>c:
+    print("a is max")
+elif b>a and b>c:
+    print("b is max")
+elif c>a and c>b:
+    print("c is max")
+else:
+    print("three are equel")
+
+
+
