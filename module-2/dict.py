d23f6d84a088ce468b62cedb85e061bf7250e9e6
DELL <nirpansuriya@gmail.com>
2024-01-02 13:35:28 +0530

    list function.py

---
+++ module-2/dict.py
@@ -0,0 +1,5 @@
+stdata={'id':7238,'name':'nirav','sub':'Python'}
+
+print(stdata)
+print(stdata['name'])
+print(stdata.get('sub'))
---
+++ module-2/list function.py
@@ -0,0 +1,79 @@
+data=['c','c++','ds','python','java','php']
+print(data)
+print(len(data))
+
+if 'c+' in data:
+    print("yes...")
+else:
+    print("no data is not found")
+
+# replace your string in index wise
+data[2]='android'
+print(data)
+
+# your list is print in for loop
+for i in data:
+    print(i)
+
+# find your list in string index
+print(data.index('android'))
+
+# print all data is index wise using index functoion
+for i in data:
+    print(f"{data.index(i)}={i}")
+
+# print all data is index wise without using index function
+n=0
+for i in data:
+    print(f"{n}={i}")
+    n+=1
+
+# add data in your list in last eliment
+data.append('database')
+print(data)
+
+# add data in your list in any space reference to index
+data.insert(2,'c#')
+print(data)
+
+# data delete in your list in last eliment defalt
+data.pop()
+print(data)
+
+# data delete in your list in index wise
+data.pop(2)
+print(data)
+
+# data delete in your list object wise
+data.remove('c')
+print(data)
+
+# data delete in index wise
+del data[0]
+print(data)
+
+# all data clear in your list
+# data.clear()
+# print(data)
+
+# print all data is reverse
+data.reverse()
+print(data)
+
+# all data in order in alphabetically
+data.sort()
+print(data)
+
+# data copy in other list
+# newdata=data.copy()
+# print(newdata)
+
+# add other list in your list without using function
+newdata=['HTML','CSS','JS','Bootstrap']
+print(newdata)
+print(data+newdata)
+
+# add other list in your list using extend() function
+data.extend(newdata)
+print(data)
+
